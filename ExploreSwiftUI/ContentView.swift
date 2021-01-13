80e491667392b851c15e693f69865a9e7d50ad80
Ashik Ahmad <ashikcu@gmail.com>
2021-01-13 19:18:34 +0600

    Initial Commit

---
+++ ExploreSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExploreSwiftUI
+//
+//  Created by Ashik Uddin on 13/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExploreSwiftUI/ExploreSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreSwiftUIApp.swift
+//  ExploreSwiftUI
+//
+//  Created by Ashik Uddin on 13/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ExploreSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
